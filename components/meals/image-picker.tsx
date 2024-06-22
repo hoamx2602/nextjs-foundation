20a255bf96a69843598be752b614f82295ff1f33
hoamx <hoamx.work@gmail.com>
2024-06-23 00:44:10 +0700

    feat: handle upload image with blob file

--- components/meals/image-picker.tsx
+++ components/meals/image-picker.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 interface ImagePickerProps {
   label: string;
@@ -8,15 +9,41 @@ interface ImagePickerProps {
 }
 
 const ImagePicker = ({ label, name }: ImagePickerProps) => {
+  const [pickedImage, setPickedImage] = useState<string | null>(
+    null
+  );
   const imageInput = useRef<HTMLInputElement>(null);
 
   function handlePickClick() {
     imageInput.current?.click();
   }
+
+  function handleImageChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (!event?.target?.files) return;
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result as string);
+    };
+    fileReader.readAsDataURL(file);
+  }
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image src={pickedImage} alt="The image selected by user." fill />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -24,6 +51,9 @@ const ImagePicker = ({ label, name }: ImagePickerProps) => {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          //   multiple // allow select mutiple files
+          onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
