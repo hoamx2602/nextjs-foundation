367bcf98b37f08912abe3ecbcfc4adbd5a8b7b9f
hoamx <hoamx.work@gmail.com>
2024-06-22 23:47:35 +0700

    feat: handle show individual meal

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,11 +1,40 @@
-const MealDetail = ({
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+const MealDetailsPage = async ({
   params,
 }: {
   params: {
     mealSlug: string;
   };
 }) => {
-  return <h1>{params.mealSlug}</h1>;
+  const meal = await getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
-export default MealDetail;
+export default MealDetailsPage;
--- lib/meals.ts
+++ lib/meals.ts
@@ -6,5 +6,11 @@ const db = sql("meals.db");
 export async function getMeals(): Promise<MealItemProps[]> {
 	await new Promise((resolve) => setTimeout(resolve, 2000));
 	// throw new Error("Some thing went wrong!");
-	return  db.prepare("SELECT * FROM meals").all() as MealItemProps[];
+	return db.prepare("SELECT * FROM meals").all() as MealItemProps[];
+}
+
+export async function getMeal(slug: string): Promise<MealItemProps> {
+	return db
+		.prepare("SELECT * FROM meals WHERE slug = ?")
+		.get(slug) as MealItemProps;
 }
